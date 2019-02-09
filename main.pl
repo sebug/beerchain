15ab658d71c73656580a8237c9c42b199c7dcc72
sebug <sebastian.gfeller@gmail.com>
2019-02-09 16:11:46 +0100

    Command line arguments

--- main.pl
+++ main.pl
@@ -1,3 +1,10 @@
 use v6;
 
-say "Welcome to the beerchain";
+sub MAIN (Str :$command = "usage",
+	  Str :$brand = "",
+	  Num :$quantity = Num(3))
+{
+    say "Welcome to the beerchain";
+    $command.say
+}
+
